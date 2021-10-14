f344fc5e5cad9e14585a2ec67fae521df47c1e2b
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-14 19:49:29 -0300

    sdfg

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 let btnEnv = document.getElementById("btnEnviar");
 
 btnEnv.addEventListener("click", () => {
-  let usuario: number = Number(usuario.value);
-  let password: number = Number(password.value);
+  let usuario: string = String(usuario.value);
+  let password: string = String(password.value);
 
-if usuario === "Juan" { 
-  if password === "ClaveJuan"{
-    console.log("Bienvenido");
-  }else{
-    console.log("Contraseña inválida");
+  if (usuario === "Juan") {
+    if (password === "ClaveJuan") {
+      console.log("Bienvenido");
+    } else {
+      console.log("Contraseña inválida");
+    }
+  } else {
+    console.log("Usuario inválido");
   }
-}
-else{
-  console.log("Usuario inválido");
-}
 });
