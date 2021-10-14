8b4673dd54094918870946683d6713785f2f5199
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-14 20:58:53 -0300

    final

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ let btnEnv = document.getElementById("btnEnviar");
 let usuario = document.getElementById("usuario");
 let password = document.getElementById("password");
 
-let u: string = usuario.value;
-let p: string = password.value;
-
 btnEnv.addEventListener("click", () => {
+  let u: string = usuario.value;
+  let p: string = password.value;
+
   if (u === "Juan") {
     if (p === "ClaveJuan") {
       console.log("Bienvenido");
