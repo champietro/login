d71488498fbe6fbe45b1b95cb8d492c1213422c3
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-14 19:47:25 -0300

    fghfgh

--- index.ts
+++ index.ts
@@ -1,16 +1,17 @@
 let btnEnv = document.getElementById("btnEnviar");
 
 btnEnv.addEventListener("click", () => {
-  let v1m: number = Number(usuario.value);
-  let v1s: number = Number(password.value);
+  let usuario: number = Number(usuario.value);
+  let password: number = Number(password.value);
 
-if promedioS >= 60{ 
-  promedioS = promedioS / 60;
-  promedioM = promedioM + promedioS;
-  promedioS = promedioS % 60;
+if usuario === "Juan" { 
+  if password === "ClaveJuan"{
+    console.log("Bienvenido");
+  }else{
+    console.log("Contraseña inválida");
+  }
 }
-
-
-  console.log("Bienvenido", horas, ":", minutos, ":", segundos);
-  console.log("El tiempo promedio es: ,"promedioH, ":", promedioM, ":", promedioS);
+else{
+  console.log("Usuario inválido");
+}
 });
