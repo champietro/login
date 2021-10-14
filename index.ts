53d2a9d3b168615c611d60673ed4f0054b9ce396
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-14 20:49:03 -0300

    fgh

--- index.ts
+++ index.ts
@@ -2,12 +2,12 @@ let btnEnv = document.getElementById("btnEnviar");
 let usuario = document.getElementById("usuario");
 let password = document.getElementById("password");
 
-let usu = string;
-let pass = string;
+let u: string = usuario.value;
+let p: string = password.value;
 
 btnEnv.addEventListener("click", () => {
-  if (usu === "Juan") {
-    if (pass === "ClaveJuan") {
+  if (u === "Juan") {
+    if (p === "ClaveJuan") {
       console.log("Bienvenido");
     } else {
       console.log("Contraseña inválida");
